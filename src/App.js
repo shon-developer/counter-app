2e83a73e1cbf92f27ccb5b3ee5ed8b771b772841
shon <shonbwd@gmail.com>
2024-05-27 10:41:18 +0530

    added readme

--- src/App.js
+++ src/App.js
@@ -15,15 +15,15 @@ function App() {
   };
   return (
     <div className="bg-black text-white w-full min-h-screen flex flex-col items-center justify-start px-6 py-2 text-4xl pt-24">
-      <h1 className="text-7xl text-cyan-500">{token}</h1>
+      <h1 className="text-7xl text-cyan-500 py-12">{token}</h1>
       <div className="flex items-center gap-6">
-        <button onClick={increment} className="text-5xl">
+        <button onClick={increment} className="text-5xl text-green-300">
           +
         </button>
         <button onClick={reset} className="text-2xl">
           Reset
         </button>
-        <button onClick={decrement} className="text-5xl">
+        <button onClick={decrement} className="text-7xl text-red-600">
           -
         </button>
       </div>
